bf393a73d379a5352548973f423ef5fd2451c3d2
thehappyt <thompson.adams@gmail.com>
2014-08-15 17:20:31 -0700

    Update gsgrid.js

--- gsgrid.js
+++ gsgrid.js
@@ -137,7 +137,6 @@
                 v1 = (i==-N)?vertex({ canvas: this.canvas, opacity: 0.5, color: vec(1,1,1) }):gps[n-2*N-1].vqd.v0
                 v2 = (i==-N)?(j==-N)?vertex({ canvas: this.canvas, opacity: 0.5, color: vec(1,1,1) }):gps[n-1].vqd.v1:gps[n-2*N-1].vqd.v3
                 v3 = (j==-N)?vertex({ canvas: this.canvas, opacity: 0.5, color: vec(1,1,1) }):gps[n-1].vqd.v0
-                console.log("Quad "+n+" has vertices "+v0.__id+", "+v1.__id+", "+v2.__id+", and "+v3.__id+".");
                 gps[n] = new GridPoint({ canvas: this.canvas, pos: center.add(rhat.multiply(i*d)+that.multiply(j*d)), grid: this, __gid: n, d: this.d, v0: v0, v1: v1, v2: v2, v3: v3 });        // , shaftwidth: this.shaftwidth
                 if ((j == N) && (i < N)) {i++; j=-N;} else j++;
             }
